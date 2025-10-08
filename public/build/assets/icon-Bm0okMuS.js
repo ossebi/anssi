d6913e95055e889c75444bd4f6bc1ccb5cf595ab
Conscet Ossebi <conscetossebi91@gmail.com>
2025-10-08 11:14:32 +0200

    js build

---
+++ public/build/assets/icon-Bm0okMuS.js
@@ -0,0 +1,1 @@
+const s="/build/assets/about-img-CQ1XVK3x-CQ1XVK3x.png",i="/build/assets/icon-CkathR8N.svg";export{i,s as v};
---
+++ public/build/assets/logo-lg-DSsbnW8O.js
@@ -0,0 +1,1 @@
+const o="/build/assets/logo-lg-CvJqY_fm.jpg";export{o as l};
